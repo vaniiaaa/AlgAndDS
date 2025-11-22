24abd082b69ea9aa5d8f942d17f691d274783682
Ivan Zhavner <zhavnervan@gmail.com>
2025-11-22 12:25:41 +0300

    Segment Tree for task

--- DS/MyTask/MyTask.cpp
+++ DS/MyTask/MyTask.cpp
@@ -1,19 +1,60 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
+
+class SegmTree
+{
+    std::vector<int> heap;
+    int n;
+
+public:
+    SegmTree(int size) : n(size), heap(4 * size, INT_MAX) {}
+    
+    void Add(int index, int val)
+    {
+        DoAdd(0, n, 1, index, val);
+    }
+    
+    int FindMin(int L, int R)
+    {
+        return DoFindMin(L, R, 0, n, 1);
+    }
+
+private:
+    int DoFindMin(int L, int R, int hl, int hr, int v)
+    {
+        if (L >= R || hr <= L || R <= hl) return INT_MAX;
+        if (L <= hl && hr <= R)
+            return heap[v];
+        int m = (hl + hr) / 2;
+        if (R <= m) 
+            return DoFindMin(L, R, hl, m, 2*v);
+        if (m <= L)
+            return DoFindMin(L, R, m, hr, 2*v + 1);
+        return std::min(DoFindMin(L, m, hl, m, 2*v), 
+                       DoFindMin(m, R, m, hr, 2*v + 1));
+    }
+    
+    void DoAdd(int L, int R, int v, int index, int val)
+    {
+        if (R - L == 1)
+        {
+            heap[v] = std::min(heap[v], val);
+            return;
+        }
+        int m = (L + R) / 2;
+        index < m ? DoAdd(L, m, 2 * v, index, val) 
+                  : DoAdd(m, R, 2 * v + 1, index, val);
+        heap[v] = std::min(heap[2 * v], heap[2 * v + 1]);
+    }
+};
 
 struct team
 {
     int x, y, z;
-    team(): x(0), y(0), z(0) {}
-    team(int a, int b, int c): x(a), y(b), z(c) {}
-    bool operator<(const team& other) const
-    {
-        if (x < other.x && y < other.y && z < other.z)
-            return true;
-        else 
-            return false;
-    }
+    team() : x(0), y(0), z(0) {}
+    team(int a, int b, int c) : x(a), y(b), z(c) {}
 };
 
 int main()
@@ -31,13 +72,13 @@ int main()
         teams[i] = temp;
     }
     int count = 0;
-    for (int i = 0; i < n; ++i)
+    std::sort(teams.begin(), teams.end(), [](const team& a, const team& b){return a.x < b.x;});
+    SegmTree tree(3*n);
+    for (const team& t: teams)
     {
-        for (int j = i; j < n; ++j)
-        {
-            if (teams[i] < teams[j])
-                ++count;
-        }
+        int min = tree.FindMin(1, t.y);
+        if (min > t.z) count++;
+        tree.Add(t.y, t.z); 
     }
     out << count;
     return 0;
